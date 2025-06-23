4a636faf136042fed349028b65d23506264ee4a5
glutesha <mail@glutesha.me>
2025-06-23 19:02:13 +0300

    src: Remove default drawing

--- src/display/display.cpp
+++ src/display/display.cpp
@@ -13,25 +13,6 @@ Display::Display(String color, int brightness, int clk, int data, int cs):
     interface.setContrast(brightness*16);
     interface.setBitmapMode(1);
     interface.clearBuffer();
-      
-    bool defaultdrawing[8][8] = {{0, 0, 0, 0, 1, 0, 0, 0},
-                                 {0, 0, 0, 1, 1, 0, 0, 0},
-                                 {0, 0, 1, 1, 1, 0, 0, 0},
-                                 {0, 1, 1, 1, 1, 1, 1, 1},
-                                 {1, 1, 1, 1, 1, 1, 1, 0},
-                                 {0, 0, 0, 1, 1, 1, 0, 0},
-                                 {0, 0, 0, 1, 1, 0, 0, 0},
-                                 {0, 0, 0, 1, 0, 0, 0, 0}};
-
-    for(int row = 0; row < 8; row++){
-        for(int col = 0; col < 8; col++){
-           interface.setDrawColor(defaultdrawing[row][col]);
-           interface.drawPixel(col, row);
-           displaystate[row][col] = defaultdrawing[row][col];
-        }
-    }
-
-    interface.sendBuffer();
 }
 
 void Display::setColor(String c){
