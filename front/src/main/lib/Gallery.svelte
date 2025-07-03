26857fd9a50afa7bafa0aeccb596c45c36e7f7c4
glutesha <mail@glutesha.me>
2025-07-04 01:38:25 +0300

    Gallery DONE!

--- front/src/main/lib/Gallery.svelte
+++ front/src/main/lib/Gallery.svelte
@@ -10,11 +10,15 @@
     })
 </script>
 
-<div class="bg-neutral-900 rounded-2xl mt-3 mb-2 overflow-hidden flex flex-col w-full min-h-95 shadow-xl shadow-black/50 max-w-[380px]">
+<div class="bg-neutral-900 rounded-2xl mt-3 mb-2 overflow-hidden flex flex-col w-full shadow-xl shadow-black/50 max-w-[380px]">
   <Delete />
   <div class="flex justify-center gap-2 flex-wrap p-4">
-    {#each $pics as pic, id}
-      <Preview id={id} display={pic} />
-    {/each}
+    {#if $pics.length === 0}
+      <p class="text-2xl text-neutral-500 text-center">Empty! I suggest you add something cool here ;3</p>
+    {:else}
+      {#each $pics as pic, id}
+        <Preview id={id} display={pic} />
+      {/each}
+    {/if}
   </div>
 </div>
