2ef0550884abf9b80fee32186758dfcaa86d5c3d
glutesha <mail@glutesha.me>
2025-07-10 01:11:24 +0300

    update default pins 2

--- src/defines.h
+++ src/defines.h
@@ -4,9 +4,9 @@
 #define CHANNEL 1
 #define HIDDEN false
 #define DISPLAY_COLOR "red" // Options: red, green, blue, orange, purple, white
-#define DATA 7
-#define CLK 5
-#define CS 6
+#define DATA 9
+#define CLK 20
+#define CS 8
 #define BRIGHTNESS 1
 #define BATTERY 3
 #define BUTTON 2
