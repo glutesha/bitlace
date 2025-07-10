fde5768ea5e824fb0c8967f3de278e5e835524a2
glutesha <mail@glutesha.me>
2025-07-10 15:22:05 +0300

    Full functionality warning

--- front/src/main/App.svelte
+++ front/src/main/App.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
   import Battery from './lib/Battery.svelte';
-import Display from './lib/Display.svelte';
-    import Erase from './lib/Erase.svelte';
-    import Tool from './lib/Tool.svelte';
-    import Settings from './lib/Settings.svelte';
-    import Gallery from './lib/Gallery.svelte';
-    import Save from './lib/Save.svelte';
-    import Export from './lib/Export.svelte';
+  import Display from './lib/Display.svelte';
+  import Erase from './lib/Erase.svelte';
+  import Tool from './lib/Tool.svelte';
+  import Settings from './lib/Settings.svelte';
+  import Gallery from './lib/Gallery.svelte';
+  import Save from './lib/Save.svelte';
 </script>
 
 <main>
   <Battery />
-  <div class="flex pt-15 flex-col items-center justify-center font-sans gap-2">
-    <p class="text-7xl font-bold text-white">Bit::lace</p>
+  <div class="flex pt-13 flex-col items-center justify-center font-sans gap-2">
+    <a href="http://192.168.4.1" class="flex flex-col items-center w-80" target="_blank" rel="noopener noreferrer">
+      <p class="text-7xl font-bold text-white">Bit::lace</p>
+      <p class="text-2xl text-center text-cyan-400">Open 192.168.4.1 in browser for full functionality</p>
+    </a>
     <div class="flex flex-row items-center gap-3">
       <img
         src="gluten.svg"
