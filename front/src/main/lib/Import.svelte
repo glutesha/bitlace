76f6db8b165fb862c4437b8ff240fd17e89aa2de
glutesha <mail@glutesha.me>
2025-07-10 10:12:44 +0300

    Gallery import ready

--- front/src/main/lib/Import.svelte
+++ front/src/main/lib/Import.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
-    import { pics } from "../saves";
-	function onFileChange(event: Event) {
+    import { pics, pushSaves } from "../saves";
+
+	async function onFileChange(event: Event) {
 		const input = event.target as HTMLInputElement;
 		const file = input.files?.[0];
 		if (!file) return;
 
 		const reader = new FileReader();
-		reader.onload = () => {
+		reader.onload = async () => {
 			try {
 				const data = JSON.parse(reader.result as string);;
+                const saves: Array<Array<Array<boolean>>> = data;
+                pics.set(saves);
+                await pushSaves();
 			} catch (err) {
 				console.error("Error loading file:", err);
 			}
 		};
-		reader.readAsText(file);
+        reader.readAsText(file);
 	}
+
 </script>
 
 <label class="bg-neutral-800 hover:bg-neutral-900 active:bg-neutral-950 rounded-3xl size-xl text-white px-5 py-3 text-3xl text-center font-bold flex-1" for="file">
